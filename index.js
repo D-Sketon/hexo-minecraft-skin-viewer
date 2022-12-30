e88c0625f43579dba9f1ee81b1e298e0863deece
D-Sketon <2055272094@qq.com>
2022-12-30 11:26:31 +0800

    refactor: remove underscore

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const path = require('path');
-const fs = require('fs');
-const _ = require('underscore');
+'use strict';
+const mcskin = require('./lib/mcskin.js')
 
-let filePath = path.resolve(__dirname, 'templates/mcskin.html');
-
-function mcskin(args) {
-  let template = fs.readFileSync(filePath).toString();
-  return _.template(template)({
-    id: 'mcskin' + ((Math.random() * 9999) | 0),
-    type: args[0],
-    content: args[1],
-    autoRotate: args[2] || true,
-    animation: args[3] || false,
-    width: args[4] || 300,
-    height: args[5] || 400
-  });
-}
-
-hexo.extend.tag.register('mcskin', mcskin, {
-  async: true
+hexo.extend.tag.register("mcskin", (args) => {
+    return mcskin(args);
 });
